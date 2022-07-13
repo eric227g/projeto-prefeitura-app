00dc65fdfbb9b94fcf67d48395c54a0ed30539a4
Douglas Ato <douglasato08@gmail.com>
2022-07-13 15:45:46 -0300

    teste

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,7 +1,5 @@
 import { FC } from 'react';
 import { PropsGroup } from './types';
-import Calendar from '../Pages/Calendar';
-import { Form } from '../Pages/Form';
 import ScreenRelato from '../Pages/ScreenRelato';
 import ScreenSugestao from '../Pages/ScreenSugestao';
 import Componente from '../Pages/Componentes/Relato';
@@ -9,8 +7,6 @@ import Componente from '../Pages/Componentes/Relato';
 const AppRoutes: FC<PropsGroup> = function ({ Drawer }: PropsGroup) {
   return (
     <Drawer.Group>
-      <Drawer.Screen name="Calendário" component={Calendar} />
-      <Drawer.Screen name="Form" component={Form} />
       <Drawer.Screen name="Relato" component={ScreenRelato} />
       <Drawer.Screen name="Sugestao" component={ScreenSugestao} />
     </Drawer.Group>
