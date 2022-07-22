c6c2febc5c1ac666667db2611a6b410da68ad288
Carlos Eduardo Bageston <carlos.e.bageston@outlook.com>
2022-07-22 15:30:57 -0300

    dasd

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import MyReactNativeForm from 'pages/formik';
+import { Teste } from 'pages/teste';
 import { FC } from 'react';
 import Routes from 'routes';
 import * as ServiceWorkerRegistration from './serviceWorkerRegistration';
@@ -5,7 +7,8 @@ import * as ServiceWorkerRegistration from './serviceWorkerRegistration';
 ServiceWorkerRegistration.register();
 
 const App: FC = function () {
-  return <Routes />
+  // return <Routes />
+  return <Teste/>
 };
 
 export default App;
